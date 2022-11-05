b7d2ce892963a3fe9ae184d2508369264aa0a29b
HalukKarakaya4js <ozgurhaluk_krky@hotmail.com>
2022-11-05 22:07:07 +0300

    pet model

---
+++ server/models/Pet.js
@@ -0,0 +1,54 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const petSchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        imgUrl: {
+            type: String,
+        },
+        bio: {
+            type: String,
+            maxLength: [
+                150,
+                '`{PATH}` Alanı (`{VALUE}`), `{MAXLENGTH}` Karakterden Az Olmalıdır'
+            ],
+        },
+        sex: {
+            type: String,
+            enum: [ "Male", "Female" ]
+        },
+        birthDay: {
+            type: Date,
+        },
+        kind: {
+            type: String
+        },
+        breed: {
+            type: String,
+            required: true
+        },
+        vaccinations: {
+            type: Array,
+            default: [],
+        },
+        careGiverHistory: {
+            type: Array,
+            default: [],
+        },
+        careHistory: {
+            type: Array,
+            default: [],
+        },
+    },
+    {
+        timestamps: true
+    }
+);
+
+const Pet = mongoose.model("Pet", petSchema);
+export default Pet;
--- server/models/User.js
+++ server/models/User.js
@@ -8,6 +8,49 @@ const userSchema = new Schema(
             type: String,
             required: true,
         },
+        identity: {
+            firstName: {
+                type: String,
+                required: true,
+                maxLength: [
+                    10,
+                    '`{PATH}` Alanı (`{VALUE}`), `{MAXLENGTH}` Karakterden Az Olmalıdır'
+                ],
+            },
+            middleName: {
+                type: String,
+                maxLength: [
+                    10,
+                    '`{PATH}` Alanı (`{VALUE}`), `{MAXLENGTH}` Karakterden Az Olmalıdır'
+                ],
+            },
+            lastName: {
+                type: String,
+                required: true,
+                maxLength: [
+                    20,
+                    '`{PATH}` Alanı (`{VALUE}`), `{MAXLENGTH}` Karakterden Az Olmalıdır'
+                ],
+            },
+            job: {
+                type: String,
+                maxLength: [
+                    30,
+                    '`{PATH}` Alanı (`{VALUE}`), `{MAXLENGTH}` Karakterden Az Olmalıdır'
+                ],
+            },
+            certificates: {
+                type: Array,
+                default: []
+            },
+            bio: {
+                type: String,
+                maxLength: [
+                    150,
+                    '`{PATH}` Alanı (`{VALUE}`), `{MAXLENGTH}` Karakterden Az Olmalıdır'
+                ],
+            },
+        },
         email: {
             type: String,
             required: true,
@@ -25,19 +68,19 @@ const userSchema = new Schema(
         },
         location: {
             required: true,
-            "country": {
+            country: {
                 type: String,
                 required: true
             },
-            "city": {
+            city: {
                 type: String,
                 required: true
             },
-            "lat": {
+            lat: {
                 type: String,
                 required: true
             },
-            "lng": {
+            lng: {
                 type: String,
                 required: true,
             },
@@ -45,10 +88,28 @@ const userSchema = new Schema(
         profileImgUrl: {
             type: String,
         },
-        pets: [
-            {
-                type: String
-            }
-        ]
+        pets: {
+            type: Array,
+            default: [],
+        },
+        isCareGiver: {
+            type: Boolean,
+            default: false,
+        },
+        pastCaregivers: {
+            type: Array,
+            default: [],
+        },
+        caregiverCareer: {
+            type: Array,
+            default: [],
+        },
+        
+    },
+    {
+        timestamps: true
     }
-)
+);
+
+const User = mongoose.model("User", userSchema);
+export default User;
