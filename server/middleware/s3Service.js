8befb0ffbbe1056517288e24c3fe76bf43411190
HalukKarakaya4js <ozgurhaluk_krky@hotmail.com>
2022-11-09 16:06:43 +0300

    fix

--- server/middleware/s3Service.js
+++ server/middleware/s3Service.js
@@ -7,19 +7,20 @@ dotenv.config();
 
 const unlinkAsync = promisify(FS.unlink);
 
-const s3Uploadv2 = async (req, res) => {
+const s3Upload = async (req, res) => {
+
   const filePath = `s3UploadBridge/${req.newFileName}`;
-  const fileStream = new FS.createReadStream(filePath);
-        
   const endpoint = new AWS.Endpoint(req.regionEndPoint);
+  const fileStream = new FS.createReadStream(filePath);
+
   const s3 = new AWS.S3(
-      {
-          endpoint: endpoint,
-          credentials: {
-              accessKeyId: process.env.IDRIVE_ACCESS_KEY,
-              secretAccessKey: process.env.IDRIVE_SECRET_ACCESS_KEY
-            } 
-      }
+    {
+        endpoint: endpoint,
+        credentials: {
+            accessKeyId: process.env.IDRIVE_ACCESS_KEY,
+            secretAccessKey: process.env.IDRIVE_SECRET_ACCESS_KEY
+          } 
+    }
   );
 
   var params = {
@@ -27,15 +28,6 @@ const s3Uploadv2 = async (req, res) => {
       Key: req.newFileName,
       Body: fileStream
   };
-
-  AWS.config.update({
-      region: 'MY_REGION',
-      apiVersion: 'latest',
-      credentials: {
-        accessKeyId: 'MY_ACCESS_KEY',
-        secretAccessKey: 'MY_SECRET_KEY'
-      }
-    })
 
   s3.putObject(params, function(err, data) {
     if(err){
@@ -47,17 +39,34 @@ const s3Uploadv2 = async (req, res) => {
       req.uploadedData = data;
       unlinkAsync(filePath).then(
         (val) => {
-          res.status(200).json(
-            {
-                error: false,
-                iDriveId: req.uploadedData,
-                objectName: req.newFileName
+          var getImgParams = {
+            Bucket: process.env.IDRIVE_PROFILE_IMG_BUCKET_NAME,
+            Key: req.newFileName,
+          };
+
+          s3.headObject(getImgParams, function(err, responseData){
+            if (err) {
+              return res.status(500).json(
+                {
+                  error: true,
+                  message: err
+                }
+              );
+            } else {
+              return res.status(200).json(
+                {
+                  error: false,
+                  iDriveId: req.uploadedData,
+                  objectName: req.newFileName,
+                  data: responseData
+                }
+              );
             }
-          );
+          });
         }
       );
     }
   });
 };
 
-export default s3Uploadv2;
+export default s3Upload;
