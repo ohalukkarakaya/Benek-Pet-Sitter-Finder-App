1d9fcd1af72e69a0b0a390ae44ec79411d673e0d
HalukKarakaya4js <ozgurhaluk_krky@hotmail.com>
2022-12-09 02:02:52 +0300

    delete vaccination refactor

--- server/routes/pet/pet.js
+++ server/routes/pet/pet.js
@@ -544,81 +544,68 @@ router.delete(
         );
       }
 
-      await Pet.findById(req.params.petId).then(
-        (pet) => {
-          if(!pet){
-            return res.status(404).json(
-              {
-                error: true,
-                message: "Pet couldn't found"
-              }
-            );
-          }
-
-          const promiseUrlDelete = urlList.map(
-            (url) => {
-              return new Promise(
-                (resolve, reject) => {
-                  const splitUrl = url.split('/');
-                  const imgName = splitUrl[splitUrl.length - 1];
+      const promiseUrlDelete = urlList.map(
+        (url) => {
+          return new Promise(
+            (resolve, reject) => {
+              const splitUrl = url.split('/');
+              const imgName = splitUrl[splitUrl.length - 1];
   
-                  const deleteImageParams = {
-                    Bucket: process.env.BUCKET_NAME,
-                    Key: `pets/${pet._id.toString()}/petsVaccinationCertificates/${imgName}`
-                  };
-                  s3.deleteObject(
-                    deleteImageParams,
-                    (error, data) => {
-                      if(error){
-                        console.log("error", error);
-                        return res.status(500).json(
-                          {
-                            error: true,
-                            message: "An error occured while deleting certificate"
-                          }
-                        );
-                      }
-
-                      pet.vaccinations = pet.vaccinations.filter(
-                        vaccination => 
-                          vaccination.fileUrl !== url
-                      );
-                      return resolve(true);
-                    }
-                  );
-                }
-              );
-            }
-          );
-
-          Promise.all(promiseUrlDelete).then(
-            (_) => {
-              const petVaccinations = pet.vaccinations;
-              pet.markModified("vaccinations");
-              pet.save(
-                (err) => {
-                  if(err){
-                    console.log("error", err);
+              const deleteImageParams = {
+                Bucket: process.env.BUCKET_NAME,
+                Key: `pets/${req.pet._id.toString()}/petsVaccinationCertificates/${imgName}`
+              };
+              s3.deleteObject(
+                deleteImageParams,
+                (error, data) => {
+                  if(error){
+                    console.log("error", error);
                     return res.status(500).json(
                       {
                         error: true,
-                        message: "An error occured while saving to database"
+                        message: "An error occured while deleting certificate"
                       }
                     );
                   }
-                }
-              );
-              return res.status(200).json(
-                {
-                  error: false,
-                  message: "vaccination deleted succesfully",
-                  remainedVaccinations: petVaccinations
+
+                  req.pet.vaccinations = pet.vaccinations.filter(
+                    vaccination => 
+                      vaccination.fileUrl !== url
+                  );
+                  return resolve(true);
                 }
               );
             }
           );
         }
-      )
+      );
+
+      Promise.all(promiseUrlDelete).then(
+        (_) => {
+          const petVaccinations = req.pet.vaccinations;
+          req.pet.markModified("vaccinations");
+          req.pet.save(
+            (err) => {
+              if(err){
+                console.log("error", err);
+                return res.status(500).json(
+                  {
+                    error: true,
+                    message: "An error occured while saving to database"
+                  }
+                );
+              }
+            }
+          );
+          return res.status(200).json(
+            {
+              error: false,
+              message: "vaccination deleted succesfully",
+              remainedVaccinations: petVaccinations
+            }
+          );
+        }
+      );
     }catch(err){
         console.log(err);
         res.status(500).json(
@@ -710,7 +697,7 @@ router.delete(
           Bucket: bucket,
           Delete: { Objects: [] }
         };
-        
+
         listedObjects.Contents.forEach(({ Key }) => {
           deleteParams.Delete.Objects.push({ Key });
         });
