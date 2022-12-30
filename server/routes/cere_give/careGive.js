c54dd5600e780adba8d4e058f4d67f5bda428850
HalukKarakaya4js <ozgurhaluk_krky@hotmail.com>
2022-12-30 23:03:26 +0300

    started to upload mission content

--- server/routes/cere_give/careGive.js
+++ server/routes/cere_give/careGive.js
@@ -1204,6 +1204,83 @@ router.put(
     }
 );
 
+router.post(
+    "/uploadMission/:careGiveId/:missionId",
+    auth,
+    async (req, res) => {
+        try{
+            const careGiveId = req.params.careGiveId;
+            const missionId = req.params.missionId;
+            const timeSignaturePassword = req.body.timeSignaturePassword;
+            const userId = req.user._id.toString();
+
+            if( !careGiveId || !missionId || !timeSignaturePassword || !userId ){
+                return res.status(400).json(
+                    {
+                        error: true,
+                        message: "missing params"
+                    }
+                );
+            }
+
+            const careGive = await CareGive.findById(careGiveId.toString());
+            if(!careGive){
+                return res.status(404).json(
+                    {
+                        error: true,
+                        message: "careGive not found"
+                    }
+                );
+            }
+            if(careGive.careGiver.careGiverId.toString !== userId){
+                return res.status(401).json(
+                    {
+                        error: true,
+                        message: "You are not authorized to upload content in this mission"
+                    }
+                );
+            }
+
+            const mission = careGive.missionCallender.find(
+                missionObject =>
+                    missionObject._id.toString() === missionId.toString()
+            );
+            if(!mission){
+                return res.status(404).json(
+                    {
+                        error: true,
+                        message: "mission not found"
+                    }
+                );
+            }
+            if(mission.missionContent.timeSignature.timePassword !== timeSignaturePassword){
+                return res.status(400).json(
+                    {
+                        error: true,
+                        message: "invalid time signature password"
+                    }
+                );
+            }
+            if(Date.parse(mission.missionContent.timeSignature.expiresAt) < Date.now()){
+                return res.status(400).json(
+                    {
+                        error: true,
+                        message: "invalid time signature password"
+                    }
+                );
+            }
+        }catch(err){
+            console.log(err);
+            return res.status(500).json(
+                {
+                    error: true,
+                    message: "Internal Server Error"
+                }
+            );
+        }
+    }
+);
+
 export default router;
 
 //TO DO: give star to care giver
