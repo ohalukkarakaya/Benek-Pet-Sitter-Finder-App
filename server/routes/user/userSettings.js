66347d7d487b3f2a1c484d75e364c52716f11f42
HalukKarakaya4js <ozgurhaluk_krky@hotmail.com>
2023-01-06 23:01:23 +0300

    delete user

--- server/routes/user/userSettings.js
+++ server/routes/user/userSettings.js
@@ -669,5 +669,35 @@ router.put(
 );
 
 //TO DO: delete user
+router.delete(
+  "deleteUser",
+  auth,
+  async (req, res) => {
+    try{
+      const userId = req.user._id.toString();
+
+      const user = await User.findById(userId);
+      if(!user){
+        return res.status(404).json(
+          {
+            error: true,
+            message: "user not found"
+          }
+        );
+      }
+
+      
+
+    }catch(err){
+      console.log("Error: delete user", err);
+      return res.status(500).json(
+        {
+          error: true,
+          message: "Internal server error"
+        }
+      );
+    }
+  }
+);
 
 export default router;
