7759d2052368e4ab3c2423aea900a16b29d6f782
HalukKarakaya4js <ozgurhaluk_krky@hotmail.com>
2023-01-09 01:25:06 +0300

    delete || deactivate user

--- server/routes/user/userSettings.js
+++ server/routes/user/userSettings.js
@@ -678,9 +678,106 @@ router.put(
   }
 );
 
-//TO DO: delete user
+//deactivate user
+router.put(
+  "/deactivate",
+  auth,
+  async (req, res) => {
+    try{
+      const userId = req.user._id.toString();
+
+      const user = await User.findById(userId);
+      if(!user || user.deactivation.isDeactive){
+        return res.status(404).json(
+          {
+            error: true,
+            message: "user not found"
+          }
+        );
+      }
+
+      const areThereAnyLinkedCareGive = await CareGive.findOne(
+        {
+          $or: [
+            {
+              "careGiver.careGiverId": user._id.toString()
+            },
+            {
+              "petOwner.petOwnerId": user._id.toString()
+            }
+          ]
+        }
+      );
+
+      if(areThereAnyLinkedCareGive){
+        return res.status(400).json(
+          {
+            error: true,
+            messaage: "You have a care give linked to you"
+          }
+        );
+      }
+
+      const userToken = await UserToken.findOne(
+        {
+            token: req.body.refreshToken,
+        }
+      );
+
+      if(userToken){
+        await userToken.remove();
+      }
+
+      if(user.deactive.isDeactive){
+        return res.status(400).json(
+          {
+            error: true,
+            message: "user is already deactive"
+          }
+        );
+      }
+
+      user.deactivation.isDeactive = true;
+      user.deactivation.deactivationDate = Date.now();
+      user.deactivation.isAboutToDelete = false;
+      user.markModified("deactivation");
+      user.save().then(
+        (_) => {
+          return res.status(200).json(
+            {
+              error: false,
+              message: "User deactivated"
+            }
+          );
+        }
+      ).catch(
+        (err) => {
+          if(err){
+            console.log(err);
+            return res.status(500).json(
+              {
+                error: true,
+                message: "Internal server error"
+              }
+            );
+          }
+        }
+      );
+    }catch(err){
+      console.log("Error: deactivate user", err);
+      return res.status(500).json(
+        {
+          error: true,
+          message: "Internal server error"
+        }
+      );
+    }
+  }
+);
+
+//delete user
 router.delete(
-  "deleteUser",
+  "/deleteUser",
   auth,
   async (req, res) => {
     try{
@@ -750,7 +847,19 @@ router.delete(
             }
           );
         }
-      ).catch();
+      ).catch(
+        (err) => {
+          if(err){
+            console.log(err);
+            return res.status(500).json(
+              {
+                error: true,
+                message: "Internal server error"
+              }
+            );
+          }
+        }
+      );
 
     }catch(err){
       console.log("Error: delete user", err);
