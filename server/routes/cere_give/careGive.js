bd300813e2bc5015459a3a7cb8d555f06bc631cd
HalukKarakaya4js <ozgurhaluk_krky@hotmail.com>
2023-01-31 23:13:51 +0300

    command line

--- server/routes/cere_give/careGive.js
+++ server/routes/cere_give/careGive.js
@@ -383,7 +383,7 @@ router.put(
                                                     }
                                                 );
                                                 if(areThereCredit){
-                                                    
+                                                    //To Do: ! take payment
                                                 }
                                         }
                                     }
