d3afd460d3e081efa822cbf78876bf27f464148f
HalukKarakaya4js <ozgurhaluk_krky@hotmail.com>
2023-04-01 10:55:56 +0300

    approve order

---
+++ server/utils/paramRequests/paymentRequests/paramAproveOrderRequest.js
@@ -0,0 +1,49 @@
+import axios from "axios";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const paramAproveOrderRequest = async (
+    orderGuid
+) => {
+
+    var subsellersShare = price - commission;
+
+    let data = `<?xml version="1.0" encoding="utf-8"?>
+        <soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">\n
+            <soap:Body>\n
+                <Pazaryeri_TP_Siparis_Onay xmlns="https://turkpos.com.tr/">\n
+                    <G>\n
+                        <CLIENT_CODE>${process.env.PARAM_CLIENT_CODE}</CLIENT_CODE>\n
+                        <CLIENT_USERNAME>${process.env.PARAM_CLIENT_USERNAME}</CLIENT_USERNAME>\n
+                        <CLIENT_PASSWORD>${process.env.PARAM_CLIENT_PASSWORD}</CLIENT_PASSWORD>\n
+                    </G>\n
+                    <PYSiparis_GUID>${orderGuid}</PYSiparis_GUID>\n
+                    <Durum>1</Durum>\n
+                </Pazaryeri_TP_Siparis_Onay>\n
+            </soap:Body>\n
+        </soap:Envelope>`;
+
+    let config = {
+        method: 'post',
+        maxBodyLength: Infinity,
+        url: process.env.PARAM_TEST_URL,
+        headers: { 
+            'Content-Type': 'text/xml;charset=UTF-8'
+        },
+        data : data
+    };
+
+    axios.request( config )
+         .then(
+            ( response ) => {
+                return JSON.stringify( response.data );
+            }
+        ).catch(
+            ( error ) => {
+                console.log(error);
+            }
+        );
+}
+
+export default paramAproveOrderRequest;
