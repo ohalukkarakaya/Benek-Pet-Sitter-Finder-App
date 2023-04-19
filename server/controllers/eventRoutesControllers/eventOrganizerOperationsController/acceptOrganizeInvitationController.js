bde32d3c299c6fad72040f1c3b1dd70ccd8c8200
HalukKarakaya4js <ozgurhaluk_krky@hotmail.com>
2023-04-19 16:21:33 +0300

    refactor organizerOperations.js

---
+++ server/controllers/eventRoutesControllers/eventOrganizerOperationsController/acceptOrganizeInvitationController.js
@@ -0,0 +1,180 @@
+import Event from "../../../models/Event/Event.js";
+import OrganizerInvitation from "../../../models/Event/Invitations/InviteOrganizer.js";
+
+const acceptOrganizeInvitationController = async (req,res) => {
+    try{
+        const invateId = req.params.invateId;
+        if(!invateId || req.params.response){
+            return res.status(400).json(
+                {
+                    error: true,
+                    message: "Missing or wrong params"
+                }
+            );
+        }
+
+        const response = req.params.response === "true";
+
+        const invitation = await OrganizerInvitation.findById(invateId);
+        if(!invitation){
+            return res.status(404).json(
+                {
+                    error: true,
+                    message: "invitation not found"
+                }
+            );
+        }
+
+        const meetingEvent = await Event.findById(invitation.eventId);
+        if(!meetingEvent){
+            return res.status(404).json(
+                {
+                    error: true,
+                    message: "meetingEvent not found"
+                }
+            );
+        }
+
+        if(meetingEvent.date <= Date.now()){
+            invitation.deleteOne().then(
+                (_) => {
+                    return res.status(400).json(
+                        {
+                            error: true,
+                            message: "This event is expired"
+                        }
+                    );
+                }
+            ).catch(
+                (error) => {
+                    console.log(error);
+                    return res.status(500).json(
+                        {
+                            error: true,
+                            message: "Internal server error"
+                        }
+                    );
+                }
+            );
+        }
+
+        const isOrganizer = invitation.invitedId === req.user._id.toString();
+        if(!isOrganizer){
+            return res.status(403).json(
+                {
+                    error: true,
+                    message: "You are not authorized to accept this invitation"
+                }
+            );
+        }
+        
+        const isAlreadyOrganizer = meetingEvent.eventOrganizers.find(userId => userId === req.user._id.toString());
+        if(isAlreadyOrganizer){
+            return res.status(400).json(
+                {
+                    error: true,
+                    message: "you are already organizer"
+                }
+            );
+        }
+
+        if(response){
+            meetingEvent.eventOrganizers = meetingEvent.eventOrganizers.push(req.user._id.toString());
+            meetingEvent.markModified("eventOrganizers");
+            meetingEvent.save(
+                (error) => {
+                    if(error){
+                        console.log(error);
+                        return res.status(500).json(
+                            {
+                                error: true,
+                                message: "Internal server error"
+                            }
+                        );
+                    }
+                }
+            );
+
+            invitation.deleteOne().then(
+                (_) => {
+                    return res.status(200).json(
+                        {
+                            error: false,
+                            message: "invitation accepted succesfully"
+                        }
+                    );
+                }
+            ).catch(
+                (error) => {
+                    console.log(error);
+                    return res.status(500).json(
+                        {
+                            error: true,
+                            message: "Internal server error"
+                        }
+                    );
+                }
+            );
+        }else{
+            invitation.deleteOne().then(
+                (_) => {
+                    return res.status(200).json(
+                        {
+                            error: false,
+                            message: "invitation rejected succesfully"
+                        }
+                    );
+                }
+            ).catch(
+                (error) => {
+                    console.log(error);
+                    return res.status(500).json(
+                        {
+                            error: true,
+                            message: "Internal server error"
+                        }
+                    );
+                }
+            );
+        }
+            
+        await new OrganizerInvitation(
+            {
+                eventAdminId: req.user._id.toString(),
+                eventId: meetingEvent._id.toString(),
+                invitedId: organizer._id.toString(),
+                eventDate: meetingEvent.date,
+            }
+        ).save().then(
+            (invitation) => {
+                return res.status(200).json(
+                    {
+                        error: false,
+                        message: `You invited user "${organizer._id.toString()}" successfully`
+                    }
+                );
+            }
+        ).catch(
+            (error) => {
+                console.log(error);
+                return res.status(500).json(
+                    {
+                        error: true,
+                        message: "Internal server error"
+                    }
+                );
+            }
+        );
+        
+    }catch(err){
+        console.log("ERROR: accept organizer invitation - ", err);
+        return res.status(500).json(
+            {
+                error: true,
+                message: "Internal server error"
+            }
+        );
+    }
+}
+
+export default acceptOrganizeInvitationController;
---
+++ server/controllers/eventRoutesControllers/eventOrganizerOperationsController/addOrganizerController.js
@@ -0,0 +1,96 @@
+import User from "../../../models/User.js";
+import Event from "../../../models/Event/Event.js";
+import OrganizerInvitation from "../../../models/Event/Invitations/InviteOrganizer.js";
+
+const addOrganizerController = async (req, res) => {
+    try{
+        const eventId = req.params.eventId;
+        const organizerId = req.body.organizerId;
+        if(!eventId || !organizerId){
+            return res.status(400).json(
+                {
+                    error: true,
+                    message: "Missing or wrong params"
+                }
+            );
+        }
+
+        const meetingEvent = await Event.findById(eventId);
+        if(!meetingEvent){
+            return res.status(404).json(
+                {
+                    error: true,
+                    message: "Event not found"
+                }
+            );
+        }
+
+        if(meetingEvent.date <= Date.now()){
+            return res.status(400).json(
+                {
+                    error: true,
+                    message: "This event is expired"
+                }
+            );
+        }
+
+        const isAdmin = meetingEvent.eventAdmin === req.user._id.toString();
+        if(!isAdmin){
+            return res.status(403).json(
+                {
+                    error: true,
+                    message: "You are not authorized to invite organizer to this event"
+                }
+            );
+        }
+
+        const organizer = await User.findById(organizerId);
+        if(!organizer || organizer.deactivation.isDeactive){
+            return res.status(404).json(
+                {
+                    error: true,
+                    message: "User not found"
+                }
+            );
+        }
+            
+        await new OrganizerInvitation(
+            {
+                eventAdminId: req.user._id.toString(),
+                eventId: meetingEvent._id.toString(),
+                invitedId: organizer._id.toString(),
+                eventDate: meetingEvent.date,
+            }
+        ).save().then(
+            (invitation) => {
+                return res.status(200).json(
+                    {
+                        error: false,
+                        message: `You invited user "${organizer._id.toString()}" successfully`
+                    }
+                );
+            }
+        ).catch(
+            (error) => {
+                console.log(error);
+                return res.status(500).json(
+                    {
+                        error: true,
+                        message: "Internal server error"
+                    }
+                );
+            }
+        );
+        
+    }catch(err){
+        console.log("ERROR: invite event organizer - ", err);
+        return res.status(500).json(
+            {
+                error: true,
+                message: "Internal server error"
+            }
+        );
+    }
+}
+
+export default addOrganizerController;
