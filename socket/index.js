02b8101eb47f7c053242004b6a554a110b9dc8f2
HalukKarakaya4js <ozgurhaluk_krky@hotmail.com>
2023-04-24 21:55:23 +0300

    web socket server

---
+++ socket/index.js
@@ -0,0 +1,75 @@
+const io = require("socket.io")(
+    8900,
+    {
+        cors:{
+            origin: '*'
+        }
+    }
+);
+
+let users = [];
+
+const addUser = ( userId, socketId ) => {
+    !users.some(
+        user => 
+            user.userId === userId
+    ) && users.psush(
+        {
+            userId,
+            socketId
+        }
+    );
+}
+
+const getUser = ( userId ) => {
+    return users.find(
+        user =>
+            user.userId === userId
+    );
+}
+
+const removeUser = ( socketId ) => {
+    users = users.filter(
+        user =>
+            user.socketId !== socketId
+    )
+}
+
+io.on(
+    "connection",
+    ( socket ) => {
+
+        //when connect
+        socket.on(
+            "addUser",
+            ( userId ) => {
+                addUser( userId, socket.id );
+            }
+        );
+
+        //send message
+        socket.on(
+            "sendMessage",
+            ( chatObject, receiverIdList ) => {
+                for( var receiverUserId in [...new Set( receiverIdList )] ){
+                    let user = getUser( receiverUserId );
+                    if( user ){
+                        io.to(user.socketId).emit(
+                            "getMessage",
+                             chatObject
+                        )
+                    }
+                }
+            }
+        );
+
+        //when disconnect
+        socket.on(
+            "disconnect",
+            () => {
+                removeUser( socket.id );
+            }
+        );
+
+    }
+);
