a3eb7f16f1abc4fc68069a7fe06652b73da0066d
HalukKarakaya4js <ozgurhaluk_krky@hotmail.com>
2023-05-08 17:56:22 +0300

    bug fix

--- server/controllers/petRoutesControllers/petControllers/getPetsByUserIdController.js
+++ server/controllers/petRoutesControllers/petControllers/getPetsByUserIdController.js
@@ -1,4 +1,4 @@
-import User from "../../../models/User";
+import User from "../../../models/User.js";
 import Pet from "../../../models/Pet.js";
 
 const getPetsByUserIdController = async ( req, res ) => {
