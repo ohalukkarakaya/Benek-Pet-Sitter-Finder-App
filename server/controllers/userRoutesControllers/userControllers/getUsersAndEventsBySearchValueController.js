3825ba01f450c149c2cfd7efc920370f87817ff1
HalukKarakaya4js <ozgurhaluk_krky@hotmail.com>
2023-05-08 10:15:36 +0300

    filter on search

--- server/controllers/userRoutesControllers/userControllers/getUsersAndEventsBySearchValueController.js
+++ server/controllers/userRoutesControllers/userControllers/getUsersAndEventsBySearchValueController.js
@@ -10,6 +10,7 @@ const getUsersAndEventsBySearchValueController = async ( req, res ) => {
         const lng = req.body.lng;
         const searchTerm = req.body.searchValue.toString();
         const userId = req.user._id.toString();
+        const filter = req.body.filter;
 
         if(
             !lat
@@ -23,185 +24,221 @@ const getUsersAndEventsBySearchValueController = async ( req, res ) => {
                 }
             );
         }
-    
-        const users = await User.aggregate(
-            [
+
+        if( 
+            filter
+            && filter !== "user" 
+            && filter !== "User"  
+            && filter !== "event" 
+            && filter !== "Event" 
+        ){
+            return res.status( 400 ).json(
                 {
-                    // İstek yapılan konum ile kullanıcı konumları arasındaki mesafeyi hesapla
-                    $addFields: {
-                        distance: {
-                        $sqrt: {
-                            $add: [
-                            { 
-                                $pow: [ 
-                                    { 
-                                        $subtract: 
-                                            [
-                                                lat, 
-                                                "$location.lat"
-                                            ] 
-                                    }, 
-                                    2 
-                                ] 
+                    error: true,
+                    message: "wrong type of filter"
+                }
+            );
+        }
+
+        let userList;
+        let eventList;
+
+        if( 
+            !filter
+            || filter === "user" 
+            || filter === "User"
+        ){
+            const users = await User.aggregate(
+                [
+                    {
+                        // İstek yapılan konum ile kullanıcı konumları arasındaki mesafeyi hesapla
+                        $addFields: {
+                            distance: {
+                            $sqrt: {
+                                $add: [
+                                { 
+                                    $pow: [ 
+                                        { 
+                                            $subtract: 
+                                                [
+                                                    lat, 
+                                                    "$location.lat"
+                                                ] 
+                                        }, 
+                                        2 
+                                    ] 
+                                },
+                                {
+                                    $pow: [ 
+                                        { 
+                                            $subtract: 
+                                                [
+                                                    lng, 
+                                                    "$location.lng"
+                                                ] 
+                                        },
+                                        2 
+                                    ] 
+                                },
+                                ],
                             },
-                            {
-                                $pow: [ 
-                                    { 
-                                        $subtract: 
-                                            [
-                                                lng, 
-                                                "$location.lng"
-                                            ] 
-                                    },
-                                    2 
-                                ] 
                             },
-                            ],
-                        },
                         },
                     },
-                },
-                // En yakın kullanıcılara öncelik ver
-                { 
-                    $sort: { 
-                        distance: 1 
-                    } 
-                },
-                // Kullanıcıları stars, followers, followingUsersOrPets, caregiverCareer ve pastCaregivers
-                // değerlerine göre tekrar sırala
-                {
-                    $sort: {
-                        "stars.star": -1,
-                        followers: -1,
-                        followingUsersOrPets: 1,
-                        caregiverCareer: -1,
-                        pastCaregivers: -1,
+                    // En yakın kullanıcılara öncelik ver
+                    { 
+                        $sort: { 
+                            distance: 1 
+                        } 
                     },
-                },
-                // deactivation kaydı kontrol et ve geçersiz kullanıcıları atla
-                {
-                    $match: {
-                        $and: [
-                            {
-                                "deactivation.isDeactive": false
-                            },
-                            {
-                                blockedUsers: {
-                                    $nin: [
-                                        userId
+                    // Kullanıcıları stars, followers, followingUsersOrPets, caregiverCareer ve pastCaregivers
+                    // değerlerine göre tekrar sırala
+                    {
+                        $sort: {
+                            "stars.star": -1,
+                            followers: -1,
+                            followingUsersOrPets: 1,
+                            caregiverCareer: -1,
+                            pastCaregivers: -1,
+                        },
+                    },
+                    // deactivation kaydı kontrol et ve geçersiz kullanıcıları atla
+                    {
+                        $match: {
+                            $and: [
+                                {
+                                    "deactivation.isDeactive": false
+                                },
+                                {
+                                    blockedUsers: {
+                                        $nin: [
+                                            userId
+                                        ]
+                                    }
+                                },
+                                {
+                                    $or: [
+                                        {
+                                            "userName": {
+                                                $regex: searchTerm,
+                                                $options: "i"
+                                            }
+                                        },
+                                        {
+                                            "identity.firstName": {
+                                                $regex: searchTerm,
+                                                $options: "i"
+                                            }
+                                        },
+                                        {
+                                            "identity.middleName": {
+                                                $regex: searchTerm,
+                                                $options: "i"
+                                            }
+                                        },
+                                        {
+                                            "identity.lastName": {
+                                                $regex: searchTerm,
+                                                $options: "i"
+                                            }
+                                        },
+                                        {
+                                            "identity.openAdress": {
+                                                $regex: searchTerm,
+                                                $options: "i"
+                                            }
+                                        }
                                     ]
                                 }
-                            },
-                            {
-                                $or: [
-                                    {
-                                        "userName": {
-                                            $regex: searchTerm,
-                                            $options: "i"
-                                        }
-                                    },
-                                    {
-                                        "identity.firstName": {
-                                            $regex: searchTerm,
-                                            $options: "i"
-                                        }
-                                    },
-                                    {
-                                        "identity.middleName": {
-                                            $regex: searchTerm,
-                                            $options: "i"
-                                        }
-                                    },
-                                    {
-                                        "identity.lastName": {
-                                            $regex: searchTerm,
-                                            $options: "i"
-                                        }
-                                    },
-                                    {
-                                        "identity.openAdress": {
-                                            $regex: searchTerm,
-                                            $options: "i"
-                                        }
-                                    }
-                                ]
-                            }
-                        ]
+                            ]
+                        },
                     },
-                },
-            ]
-        );
-        
-        // prepare events
-        const events = await Event.find(
-            {
-                isPrivate: isPrivate === "false",
-                "adress.lat": { 
-                    $exists: true 
-                },
-                "adress.long": { 
-                    $exists: true 
-                },
-                date: { 
-                    $gte: new Date() 
-                },
-                $or: [
-                    { 
-                        "adress.adressDesc": { 
-                            $regex: searchTerm, 
-                            $options: "i" 
-                        } 
-                    },
-                    { 
-                        desc: { 
-                            $regex: searchTerm, 
-                            $options: "i" 
-                        } 
-                    }
                 ]
-            }
-        ).sort(
-            {
-                "adress.lat": 1,
-                "adress.long": 1,
-                date: 1,
-            }
-        );
+            );
 
-        events.filter(
-            ( event ) => {
+            userList = users;
+        }
 
-                if( event.maxGuests !== -1 ){
-
-                    return event.maxGuests >= event.willJoin
-                                                .length;
-
-                }else{
-
-                    return true
-
+        if(
+            !filter
+            || filter === "event"
+            || filter === "Event"
+        ){
+            // prepare events
+            const events = await Event.find(
+                {
+                    isPrivate: isPrivate === "false",
+                    "adress.lat": { 
+                        $exists: true 
+                    },
+                    "adress.long": { 
+                        $exists: true 
+                    },
+                    date: { 
+                        $gte: new Date() 
+                    },
+                    $or: [
+                        { 
+                            "adress.adressDesc": { 
+                                $regex: searchTerm, 
+                                $options: "i" 
+                            } 
+                        },
+                        { 
+                            desc: { 
+                                $regex: searchTerm, 
+                                $options: "i" 
+                            } 
+                        }
+                    ]
                 }
-            }
-        );
+            ).sort(
+                {
+                    "adress.lat": 1,
+                    "adress.long": 1,
+                    date: 1,
+                }
+            );
 
-        const sortedEvents = events.map(
-            ( event ) => {
-                const distance = Math.pow(
-                                    event.adress.lat - lat, 
-                                    2
-                                ) 
-                                + Math.pow(
-                                    event.adress.long - lng, 
-                                    2
-                                );
-                return { ...event.toObject(), distance };
-            }
-        ).sort(
-            ( a, b ) => 
-                a.distance - b.distance
-        );
+            events.filter(
+                ( event ) => {
 
-        const mergedList = [...users, ...sortedEvents];
+                    if( event.maxGuests !== -1 ){
+
+                        return event.maxGuests >= event.willJoin
+                                                    .length;
+
+                    }else{
+
+                        return true
+
+                    }
+                }
+            );
+
+            const sortedEvents = events.map(
+                ( event ) => {
+                    const distance = Math.pow(
+                                        event.adress.lat - lat, 
+                                        2
+                                    ) 
+                                    + Math.pow(
+                                        event.adress.long - lng, 
+                                        2
+                                    );
+                    return { ...event.toObject(), distance };
+                }
+            ).sort(
+                ( a, b ) => 
+                    a.distance - b.distance
+            );
+
+            eventList = sortedEvents;
+        }
+        
+        
+
+        const mergedList = [...userList, ...eventList];
 
         mergedList.forEach(
             async ( item ) => {
