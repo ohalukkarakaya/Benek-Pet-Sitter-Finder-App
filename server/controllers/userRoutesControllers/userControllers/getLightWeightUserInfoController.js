72244f6abc0c6940aebf38c2107e31cc9d8a06e6
HalukKarakaya4js <ozgurhaluk_krky@hotmail.com>
2023-05-23 00:22:34 +0300

    get user lightweight info bug fix

--- server/controllers/userRoutesControllers/userControllers/getLightWeightUserInfoController.js
+++ server/controllers/userRoutesControllers/userControllers/getLightWeightUserInfoController.js
@@ -2,62 +2,68 @@ import User from "../../../models/User.js";
 
 const getLightWeightUserInfoController = async ( req, res ) => {
     try{
-        const sendedUserIdList = req.body.userIdList;
-        if( !userIdList ){
-            return res.status( 400 ).json(
-                {
-                    error: true,
-                    message: "Missing Params"
-                }
-            );
+        const sendedUserIdList = req.body
+                                    .userIdList;
+        if( !sendedUserIdList ){
+            return res.status( 400 )
+                      .json(
+                          {
+                              error: true,
+                              message: "Missing Params"
+                          }
+                      );
         }
 
-        const userIdList = [...new Set( sendedUserIdList )];
-        userIdList.forEach(
-            async ( userId ) => {
-                const user = await User.findById( userId );
-                if( !user ){
-                  userId.remove();  
-                }else{
-                    const userrInfo = {
-                    
-                        userId: user._id
-                                    .toString(),
-                        userProfileImg: user.profileImg
-                                            .imgUrl,
-                        username: user.userName,
-                        userFullName: `${
-                                user.identity
-                                    .firstName
-                            } ${
-                                user.identity
-                                    .middleName
-                            } ${
-                                user.identity
-                                    .lastName
-                            }`.replaceAll( "  ", " ")
-                    }
+        const userIdList = [...new Set( sendedUserIdList )].map( String );
+        let updatedUserIdList = [];
 
-                    userId = userrInfo;
-                }
-            }
-        );
+        for( let userId of userIdList ){
 
-        return res.status( 200 ).json(
-            {
-                error: false,
-                message: "User info prepared succesfully",
-                userList: userIdList
+            const user = await User.findById( userId );
+
+            if( !user ){
+                continue;
             }
-        );
+
+            const userInfo = {
+                userId: user._id
+                            .toString(),
+                userProfileImg: user.profileImg
+                                    .imgUrl,
+                username: user.userName,
+                userFullName: `${
+                                  user.identity
+                                      .firstName
+                               } ${
+                                  user.identity
+                                      .middleName
+                               } ${
+                                  user.identity
+                                      .lastName
+                               }`.replaceAll("undefined", "")
+                                 .replaceAll("  ", " "),
+            };
+
+            updatedUserIdList.push( userInfo );
+        }
+
+        return res.status( 200 )
+                  .json(
+                      {
+                          error: false,
+                          message: "User info prepared succesfully",
+                          userList: updatedUserIdList
+                      }
+                  );
     }catch( err ){
         console.log( "ERROR: getLightWeightUserInfoController - ", err );
-        return res.status( 500 ).json(
-            {
-                error: true,
-                message: "Internal server error"
-            }
-        );
+        return res.status( 500 )
+                  .json(
+                        {
+                            error: true,
+                            message: "Internal server error"
+                        }
+                  );
     }
 }
 
