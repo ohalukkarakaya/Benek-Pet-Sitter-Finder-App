72c66b3d571e69a140a47ca29191de7079f6ec1b
HalukKarakaya4js <ozgurhaluk_krky@hotmail.com>
2023-08-15 23:31:41 +0300

    Update getPetKeywordsController.js

--- server/controllers/key_wordsRoutesControllers/animalCategoryControllers/getPetKeywordsController.js
+++ server/controllers/key_wordsRoutesControllers/animalCategoryControllers/getPetKeywordsController.js
@@ -10,20 +10,40 @@ const getPetKeywordsController = async (req, res) => {
 
         if(userLanguage === "tr"){
             const trResponse = [];
-            for(var i = 0; i < petDataset.pets.length; i ++){
-                var petGeneralId = petDataset.pets[i].id;
-                var petName = petDataset.pets[i].name.tr;
+            for(
+                var i = 0; 
+                i < petDataset.pets
+                              .length; 
+                i ++
+            ){
+                var petGeneralId = petDataset.pets[ i ]
+                                             .id;
+
+                var petName = petDataset.pets[ i ]
+                                        .name
+                                        .tr;
 
                 var petSpecies = [];
-                for(var index = 0; index < petDataset.pets[i].species.length; index ++){
-                    var speciesId = petDataset.pets[i].species[index].id;
-                    var speciesName = petDataset.pets[i].species[index].tr;
+                for(
+                    var index = 0; 
+                    index < petDataset.pets[ i ]
+                                      .species
+                                      .length; 
+                    index ++
+                ){
+                    var speciesId = petDataset.pets[ i ]
+                                              .species[ index ]
+                                              .id;
+
+                    var speciesName = petDataset.pets[ i ]
+                                                .species[ index ]
+                                                .tr;
 
                     var speciesObject = {
                         "id": speciesId,
                         "name": speciesName, 
                     };
-                    petSpecies.push(speciesObject);
+                    petSpecies.push( speciesObject );
                 }
 
                 var trResponseObject = {
@@ -31,24 +51,41 @@ const getPetKeywordsController = async (req, res) => {
                     "pet": petName,
                     "species": petSpecies
                 };
-                trResponse.push(trResponseObject);
+                trResponse.push( trResponseObject );
             }
 
-            return res.status(200).json(
-                {
-                    trResponse
-                }
-            );
+            return res.status( 200 )
+                      .json(
+                          {
+                              trResponse
+                          }
+                      );
         }else{
             const enResponse = [];
-            for(var i = 0; i < petDataset.pets.length; i ++){
-                var petGeneralId = petDataset.pets[i].id;
+            for(
+                var i = 0; 
+                i < petDataset.pets
+                              .length; 
+                i ++
+            ){
+                var petGeneralId = petDataset.pets[ i ].id;
                 var petName = petDataset.pets[i].name.en;
 
                 var petSpecies = [];
-                for(var index = 0; index < petDataset.pets[i].species.length; index ++){
-                    var speciesId = petDataset.pets[i].species[index].id;
-                    var speciesName = petDataset.pets[i].species[index].en;
+                for(
+                    var index = 0; 
+                    index < petDataset.pets[ i ]
+                                      .species
+                                      .length; 
+                    index ++
+                ){
+                    var speciesId = petDataset.pets[ i ]
+                                              .species[ index ]
+                                              .id;
+
+                    var speciesName = petDataset.pets[ i ]
+                                                .species[ index ]
+                                                .en;
 
                     var speciesObject = {
                         "id": speciesId,
@@ -62,23 +99,25 @@ const getPetKeywordsController = async (req, res) => {
                     "pet": petName,
                     "species": petSpecies
                 };
-                enResponse.push(trResponseObject);
+                enResponse.push( trResponseObject );
             }
 
-            return res.status(200).json(
-                {
-                    enResponse
-                }
-            );
+            return res.status( 200 )
+                      .json(
+                            {
+                                enResponse
+                            }
+                      );
         }
-    }catch(err){
-        console.log(err);
-        res.status(500).json(
-          {
-            error: true,
-            message: "Internal Server Error"
-          }
-        );
+    }catch( err ){
+        console.log( "ERROR: getPetKeywordsController - ", err );
+        res.status( 500 )
+           .json(
+            {
+                error: true,
+                message: "Internal Server Error"
+            }
+           );
       }
 }
 
