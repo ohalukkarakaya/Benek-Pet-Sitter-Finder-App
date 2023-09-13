fc68b71c8efeed0a889959b63a8b9272dd1b9cce
HalukKarakaya4js <ozgurhaluk_krky@hotmail.com>
2023-09-13 22:53:34 +0300

    Update deleteCareGiveCertificateController.js

--- server/controllers/userRoutesControllers/userSettingsControllers/deleteCareGiveCertificateController.js
+++ server/controllers/userRoutesControllers/userSettingsControllers/deleteCareGiveCertificateController.js
@@ -1,10 +1,7 @@
 import User from "../../../models/User.js";
-import s3 from "../../../utils/s3Service.js";
 
-import dotenv from "dotenv";
+import deleteFileHelper from "../../../utils/fileHelpers/deleteFileHelper.js";
 
-dotenv.config();
-const env = process.env;
 const deleteCareGiveCertificateController = async ( req, res ) => {
     try{
         const userId = req.user._id.toString();
@@ -23,94 +20,68 @@ const deleteCareGiveCertificateController = async ( req, res ) => {
 
         const user = await User.findById( userId );
   
-        const promiseUrlDelete = urlList.map(
-          ( url ) => {
-            return new Promise(
-              (
-                resolve, 
-                reject
-              ) => {
-                const splitUrl = url.split('/');
-                const imgName = splitUrl[ 
-                                      splitUrl.length - 1 
-                                ];
-    
-                const deleteImageParams = {
-                  Bucket: env.BUCKET_NAME,
-                  Key: `profileAssets/${ 
-                                          userId 
-                                      }/careGiveCertificates/${
-                                                                imgName
-                                                            }`
-                };
-                s3.deleteObject(
-                  deleteImageParams,
-                  ( error, data ) => {
-                    if( error ){
-                      console.log("error", error);
-                      return res.status( 500 )
-                                .json(
-                                  {
-                                    error: true,
-                                    message: "An error occured while deleting certificate"
-                                  }
-                                );
-                    }
-  
-                    user.identity
-                        .certificates = user.identity
-                                            .certificates
-                                            .filter(
-                                                vaccination => 
-                                                    vaccination.fileUrl !== url
-                                             );
-                    return resolve( true );
-                  }
-                );
-              }
-            );
+        for(
+          let url
+          of urlList
+        ){
+          const deleteCertificate = await deleteFileHelper( url );
+          if( deleteCertificate.error ){
+
+            return res.status( 500 )
+                      .json(
+                          {
+                            error: true,
+                            message: "Internal Server Error"
+                          }
+                       );
+          }
+
+          user.identity
+              .certificates = user.identity
+                                  .certificates
+                                  .filter(
+                                    certificate => 
+                                      certificate.fileUrl !== url
+                                   );
+        }
+
+        const certificates = user.identity
+                                 .certificates;
+
+        user.markModified( "identity" );
+        user.save(
+          ( err ) => {
+            if( err ){
+              console.log( "error", err );
+              return res.status( 500 )
+                        .json(
+                            {
+                              error: true,
+                              message: "An error occured while saving to database"
+                            }
+                         );
+            }
           }
         );
-  
-        Promise.all( promiseUrlDelete )
-               .then(
-                  (_) => {
-                    const certificates = user.identity
-                                             .certificates;
 
-                    user.markModified( "identity" );
-                    user.save(
-                      ( err ) => {
-                        if( err ){
-                          console.log( "error", err );
-                          return res.status( 500 )
-                                    .json(
-                                      {
-                                        error: true,
-                                        message: "An error occured while saving to database"
-                                      }
-                                    );
-                        }
+        return res.status( 200 )
+                  .json(
+                      {
+                        error: false,
+                        message: "certificates deleted succesfully",
+                        remainedCertificates: certificates
                       }
-                    );
-                    return res.status( 200 )
-                              .json(
-                                {
-                                  error: false,
-                                  message: "certificate deleted succesfully",
-                                  remainedVaccinations: certificates
-                                }
-                              );
-                  }
-                );
+                   );
+
     }catch( err ){
-        console.log("ERROR: deleteCareGiveCertificateController - ", err);
-        return res.status(500).json(
-            {
-                error: true,
-                message: "Internal server error"
-            }
-        );
+        console.log( "ERROR: deleteCareGiveCertificateController - ", err );
+        return res.status( 500 )
+                  .json(
+                        {
+                            error: true,
+                            message: "Internal server error"
+                        }
+                   );
     }
 }
 
