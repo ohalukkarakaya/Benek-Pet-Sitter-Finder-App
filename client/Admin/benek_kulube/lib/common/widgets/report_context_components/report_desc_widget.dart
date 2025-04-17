586e4d9a80e1d240467983602f82db85cdc585be
HalukKarakaya4js <ozgurhaluk_krky@hotmail.com>
2025-04-17 16:31:26 +0200

    long desc bug fix

--- client/Admin/benek_kulube/lib/common/widgets/report_context_components/report_desc_widget.dart
+++ client/Admin/benek_kulube/lib/common/widgets/report_context_components/report_desc_widget.dart
@@ -1,7 +1,10 @@
 import 'package:benek_kulube/data/models/user_profile_models/user_info_model.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import '../../constants/app_colors.dart';
+import '../../utils/benek_string_helpers.dart';
+import '../../utils/benek_toast_helper.dart';
 import '../text_with_profile_img.dart';
 
 class ReportDescWidget extends StatelessWidget {
@@ -16,20 +19,30 @@ class ReportDescWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
-      decoration: BoxDecoration(
-        color: Colors.black,
-        borderRadius: BorderRadius.all(Radius.circular(6.0)),
-        border: Border.all(
-          color: AppColors.benekWarningOrange,
-          width: 1.0,
+    return GestureDetector(
+      onLongPress: () {
+        Clipboard.setData(ClipboardData(text: reportDesc));
+        BenekToastHelper.showSuccessToast(
+            BenekStringHelpers.locale('operationSucceeded'),
+            BenekStringHelpers.locale('copied'),
+            context
+        );
+      },
+      child: Container(
+        width: double.infinity,
+        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
+        decoration: BoxDecoration(
+          color: Colors.black,
+          borderRadius: BorderRadius.all(Radius.circular(6.0)),
+          border: Border.all(
+            color: AppColors.benekWarningOrange,
+            width: 1.0,
+          ),
         ),
-      ),
-      child: TextWithProfileImg(
-          text: reportDesc,
-          profileImg: reportOwner.profileImg!
+        child: TextWithProfileImg(
+            text: reportDesc,
+            profileImg: reportOwner.profileImg!
+        ),
       ),
     );
   }
