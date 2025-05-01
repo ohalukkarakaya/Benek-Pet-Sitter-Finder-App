8c9110c11354bdade5c76aa736614cb24ffdfc9a
HalukKarakaya4js <ozgurhaluk_krky@hotmail.com>
2025-05-01 23:19:55 +0200

    npm start script fix

--- server/utils/getLightWeightPetInfoHelper.js
+++ server/utils/getLightWeightPetInfoHelper.js
@@ -1,4 +1,4 @@
-import User from "../models/user.js";
+import User from "../models/User.js";
 
 import getPetKindHelper from "./getPetKindHelper.js";
 import getLightWeightUserInfoHelper from "./getLightWeightUserInfoHelper.js";
